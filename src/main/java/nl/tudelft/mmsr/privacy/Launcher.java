4f14adef975f3e1d08fa7a6a64848f55867b9550
Jeroen Vrijenhoef <jeroen@vrijenhoef.net>
2017-06-13 16:20:44 +0200

    created project template

---
+++ src/main/java/nl/tudelft/mmsr/privacy/Launcher.java
@@ -0,0 +1,10 @@
+package nl.tudelft.mmsr.privacy;
+
+/**
+ * Created by Jeroen Vrijenhoef on 6/13/17.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Ello");
+    }
+}
