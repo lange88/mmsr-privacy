07230fdfe9b5aad02ff242891ec3a2079fd52a69
Jeroen Vrijenhoef <jeroen@vrijenhoef.net>
2017-07-02 20:10:22 +0200

    added face detection benchmarking function

--- src/test/java/nl/tudelft/mmsr/privacy/BenchmarkingTest.java
+++ src/test/java/nl/tudelft/mmsr/privacy/BenchmarkingTest.java
@@ -3,6 +3,7 @@ package nl.tudelft.mmsr.privacy;
 import javafx.scene.image.ImageView;
 import nl.tudelft.mmsr.privacy.detection.FaceDetection;
 import nl.tudelft.mmsr.privacy.detection.FaceDetectionFactory;
+import nl.tudelft.mmsr.privacy.detection.FaceDetectionStrategy;
 import nl.tudelft.mmsr.privacy.detection.HaarCascadeStrategy;
 import nl.tudelft.mmsr.privacy.encryption.AESDecryptionStrategy;
 import nl.tudelft.mmsr.privacy.encryption.AESEncryptionStrategy;
@@ -12,6 +13,9 @@ import org.apache.commons.io.FileUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.imgcodecs.Imgcodecs;
 
 import java.io.File;
 import java.util.Iterator;
@@ -29,6 +33,9 @@ public class BenchmarkingTest {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
     }
 
+    /**
+     * Effectively also does face detection, so total time also depends on this.
+     */
     @Test
     public void testEncryptionSpeedAES() {
         String testDir = System.getProperty("user.dir") +  "/src/test/java/resources/images_single_face";
@@ -43,7 +50,9 @@ public class BenchmarkingTest {
         int detected_faces = 0;
         int total_faces = 450;
 
-        // loop through all the test images
+        long tStart = System.currentTimeMillis();
+
+        // loop through all the test images (450 total)
         Iterator it = FileUtils.iterateFiles(new File(testDir), null, false);
         while(it.hasNext()){
             File image = (File) it.next();
@@ -52,5 +61,55 @@ public class BenchmarkingTest {
             detection.setImageResult(new ImageView());
             detection.detectFaces();
         }
+
+        long tEnd = System.currentTimeMillis();
+        long tDelta = tEnd - tStart;
+        double elapsedSeconds = tDelta / 1000.0;
+        double avgSeconds = elapsedSeconds / 450.0;
+
+        System.out.println("Total time elapsed: " + elapsedSeconds + " avg " + avgSeconds + " seconds.");
+    }
+
+    /**
+     * Only finds the faces, does no encryption
+     */
+    @Test
+    public void testFaceDetectionSpeed() {
+        String testDir = System.getProperty("user.dir") +  "/src/test/java/resources/images_single_face";
+
+        FaceDetectionStrategy strategy = new HaarCascadeStrategy();
+
+        int processed_images = 0;
+        int total_images = 0;
+        int detected_faces = 0;
+        int total_faces = 450;
+        int total_wrong_detections = 0; // we know each image should only contain 1 face
+
+        long tStart = System.currentTimeMillis();
+
+        // loop through all the test images
+        Iterator it = FileUtils.iterateFiles(new File(testDir), null, false);
+        while(it.hasNext()){
+            File imageFile = (File) it.next();
+            Mat image = Imgcodecs.imread(imageFile.getAbsolutePath());
+
+            MatOfRect faceDetections = strategy.detectFaces(image);
+
+            int numberOfFaces = faceDetections.toArray().length;
+            if (numberOfFaces == 0) {
+                total_wrong_detections += 1;
+            }
+            else if (numberOfFaces > 1) {
+                total_wrong_detections += numberOfFaces;
+            }
+        }
+
+        long tEnd = System.currentTimeMillis();
+        long tDelta = tEnd - tStart;
+        double elapsedSeconds = tDelta / 1000.0;
+        double avgSeconds = elapsedSeconds / 450.0;
+
+        System.out.println("Total time elapsed: " + elapsedSeconds + " avg " + avgSeconds + " seconds\n" +
+                "Wrong detections: " + total_wrong_detections);
     }
 }
